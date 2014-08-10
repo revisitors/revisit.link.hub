29d4ed71d26ba2147b9f1adf6d72c44d87dfd0c2
Jen <jen@meatspac.es>
2014-08-10 12:49:29 -0700

    added clientside js

---
+++ public/js/main.js
@@ -0,0 +1,27 @@
+$(function() {
+  var services = [];
+  var servicesEl = $('#services');
+  var servicesAdd = $('.services-added');
+  var maxLimit = $('.max-limit');
+
+  $('.online').click(function () {
+    if (services.length < 4) {
+      services.push($(this).find('.url').text());
+
+      servicesAdd.text(services.join(' => '));
+      servicesEl.val(services.join(','));
+    } else {
+      maxLimit.addClass('on');
+
+      setTimeout(function () {
+        maxLimit.removeClass('on');
+      }, 2500);
+    }
+  });
+
+  $('.reset').click(function () {
+    services = [];
+    servicesAdd.empty();
+    servicesEl.val('');
+  });
+});
