10eb51ab9d64f45e1bf67def79a2bb740ba43c86
Gianni Chiappetta <gianni@runlevel6.org>
2014-08-25 06:08:18 -0400

    Do a design

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,46 @@
-var $ = require('jquery');
+var $         = require('jquery');
 var ImageView = require('imageview');
 
-var services = [];
-var servicesEl = $('#services');
+var services    = [];
+var servicesEl  = $('#services');
 var servicesAdd = $('.services-added');
-var maxLimit = $('.max-limit');
+var maxLimit    = $('.max-limit');
 var serviceList = $('#service-list');
-var create = $('#create');
-var body = $('body');
-var form = $('form');
-var loading = $('#loading');
-var fileAdded = false;
+var create      = $('#create');
+var body        = $('body');
+var form        = $('form');
+var loading     = $('#loading');
+var fileAdded   = false;
+var token       = $('.token');
 
 var iv = new ImageView({
   quality: 0.5,
-  maxSize: 600
+  maxSize: 800
 });
 
 iv.preview();
 
+var renderSelectedServices = function (services) {
+  var serviceItems = $.map(services, function (s, idx) {
+    return '<li data-index="' + idx + '">' + s.title + '</li>';
+  });
+  var serviceData = $.map(services, function (s, idx) {
+    return s.url;
+  });
+
+  servicesAdd.toggleClass('active', !!services.length).html(serviceItems);
+  servicesEl.val(serviceData.join(','));
+};
+
 var checkValid = function () {
   if (services.length > 0 && fileAdded) {
-    create.addClass('on');
-  } else {
-    create.removeClass('on');
+    create.removeAttr('disabled');
+  }
+  else {
+    create.attr('disabled', true);
   }
 };
+checkValid();
 
 body.on('change', '#photo-picker', function () {
   fileAdded = true;
@@ -34,32 +49,47 @@ body.on('change', '#photo-picker', function () {
 
 body.on('click', '.online', function () {
   if (services.length < 4) {
-    services.push($(this).find('.url').text());
+    services.push($(this).data());
+    renderSelectedServices(services);
+  }
+  else {
+    servicesAdd.addClass('alert');
 
-    servicesAdd.text(services.join(' => '));
-    servicesEl.val(services.join(','));
+    setTimeout(function () {
+      servicesAdd.removeClass('alert');
+    }, 2000);
   }
 
   checkValid();
 });
 
+servicesAdd.on('click', 'li', function () {
+  var idx = parseInt(this.getAttribute('data-index'), 10);
+  services.splice(idx, 1);
+  renderSelectedServices(services);
+});
+
 $.get('/services', function (data) {
   $('#token').val(data.token);
+  token.text(data.token);
   serviceList.empty();
 
   data.services.forEach(function (d) {
-    var li = $('<li><span class="status"> &#9733;</span></li>');
-    var url = $('<span class="url"></span>');
-    var description = $('<p class="description"><span></span><img></p>');
+    var li          = $('<li class="service-list-item"></li>');
+    var preview     = $('<div class="image-preview"><img /></div>');
+    var title       = $('<h4 />');
+    var url         = $('<div class="url" />');
+
+    li.data({ title: d.description, url: d.url });
+    preview.find('img').attr('src', d.sample);
+    title.text(d.description);
     url.text(d.url);
-    description.find('span').text(d.description);
-    description.find('img').attr('src', d.sample);
 
     if (d.online) {
       li.addClass('online');
     }
 
-    li.append(url).append(description);
+    li.append(preview, title, url);
     serviceList.append(li);
   });
 });
@@ -71,7 +101,9 @@ form.on('submit', function () {
 
 body.on('click', '#reset', function () {
   services = [];
-  servicesAdd.empty();
+  renderSelectedServices(services);
   servicesEl.val('');
   create.removeClass('on');
+  $('#preview').empty();
+  $('.content').val('');
 });
